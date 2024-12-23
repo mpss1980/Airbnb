9a6302fd89f5f80007e87fce02ca8f2e921d9def
Marcos Sousa <marcosp.sousa@gmail.com>
2024-12-23 17:48:37 -0300

    Finished detail view

--- Airbnb/Core/Components/Rating.swift
+++ Airbnb/Core/Components/Rating.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct Rating: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 2) {
+            Image(systemName: "star.fill")
+            Text("4.86")
+        }
+        .foregroundStyle(.black)
     }
 }
 
--- Airbnb/Core/Listing/ListingItemView.swift
+++ Airbnb/Core/Listing/ListingItemView.swift
@@ -8,50 +8,37 @@
 import SwiftUI
 
 struct ListingItemView: View {
-    
-    var images = [
-        "listing-1",
-        "listing-2",
-        "listing-3",
-        "listing-4",
-    ]
-    
+
     var body: some View {
         VStack(spacing: 8) {
-            TabView {
-                ForEach(images, id: \.self) { image in
-                    Image(image)
-                        .resizable()
-                        .scaledToFill()
-                }
-            }
-            .frame(height: 320)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .tabViewStyle(.page)
-            
+            ListingImageCarouselView()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+
             HStack(alignment: .top) {
                 VStack(alignment: .leading) {
                     Text("Miami, Florida")
                         .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+
                     Text("12 mi away")
                         .foregroundStyle(.gray)
+
                     Text("Nov 3 - 10")
                         .foregroundStyle(.gray)
-                    
-                    HStack() {
+
+                    HStack {
                         Text("$567")
                             .fontWeight(.semibold)
                         Text("night")
                     }
+                    .foregroundStyle(.black)
                 }
-                
+
                 Spacer()
-                
-                HStack(spacing: 2) {
-                    Image(systemName: "star.fill")
-                    Text("4.86")
-                }
-               
+
+                Rating()
+
             }
             .font(.footnote)
         }
